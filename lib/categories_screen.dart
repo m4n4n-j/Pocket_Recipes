caa23a573fb75a9e47be0c351ade385f16d82b3b
Manan <manan19jain@gmail.com>
2020-08-19 12:03:10 +0530

    added fonts and colors in ThemeData

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Pocket Recipes"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(15),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
