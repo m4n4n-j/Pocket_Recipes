a652d62a18886623c2bb3a94b692c4dc995e58e5
Manan <manan19jain@gmail.com>
2020-08-19 11:42:44 +0530

    basic grid design ready

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,17 +6,22 @@ import 'category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Pocket Recipes"),
       ),
-      children: DUMMY_CATEGORIES
-          .map((catData) =>
-              CategoryItem(title: catData.title, color: catData.color))
-          .toList(),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) =>
+                CategoryItem(title: catData.title, color: catData.color))
+            .toList(),
+      ),
     );
   }
 }
